1398286bef17360d3775c9a5d9cfcfac57952d89
Savanii <tejashree1215@gmail.com>
2026-07-09 15:08:01 +0530

    Daily MIS 1.0

--- modules/RP01/RP01/__init__.py
+++ modules/RP01/RP01/__init__.py
@@ -1,10 +1,27 @@
+# from flask import Blueprint
+
+# MODULE_INFO = {
+#     'code': 'RP01',
+#     'name': 'Reports'
+# }
+
+# bp = Blueprint('RP01', __name__, template_folder='.')
+
+# from . import views
+
+print("LOADED:", __file__)
 from flask import Blueprint
 
 MODULE_INFO = {
-    'code': 'RP01',
-    'name': 'Reports'
+    "code": "RP01",
+    "name": "Reports"
 }
 
-bp = Blueprint('RP01', __name__, template_folder='.')
+bp = Blueprint(
+    "RP01",
+    __name__,
+    template_folder="."
+)
 
 from . import views
+from .JJLTPL import jjltpl
