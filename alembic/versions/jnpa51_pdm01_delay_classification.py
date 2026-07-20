913e26566c46e365b0c5b296bcebda82141628c8
Shubham Shinde <shubhamshindesunil@gmail.com>
2026-07-20 14:45:27 +0530

    Rename revision ID and update classification name in migration script

--- alembic/versions/jnpa51_pdm01_delay_classification.py
+++ alembic/versions/jnpa51_pdm01_delay_classification.py
@@ -1,6 +1,6 @@
 """Rename PDM01 classifications and add description.
 
-Revision ID: jnpa51_pdm01_delay_classification
+Revision ID: jnpa51_pdm01_delay_classes
 Revises: jnpa50_pdm01_type_3_type_4
 Create Date: 2026-07-20
 """
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 from alembic import op
 
 
-revision: str = 'jnpa51_pdm01_delay_classification'
+revision: str = 'jnpa51_pdm01_delay_classes'
 down_revision: Union[str, None] = 'jnpa50_pdm01_type_3_type_4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
